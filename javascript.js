f87eeec4084b1e4994c5478dccd68117fdde013a
shadowfax29 <ravi.kumargowda2429@gmail.com>
2023-09-14 12:24:57 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+let inputbox=document.getElementById("search");
+let list=document.getElementById("list-container");
+
+function add(){
+    if(inputbox===""){
+        alert("its empty");
+    }
+    else{
+        let li=document.createElement('li');
+        li.innerHTML=inputbox.value;
+        list.appendChild(li);
+        let span=document.createElement('span');
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value="";
+    savedata();
+}
+list.addEventListener('click',function(e){
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName==='SPAN'){
+        e.target.parentElement.remove();
+        savedata();
+    }
+
+},false)
+
+function savedata(){
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function showtask(){
+    list.innerHTML=localStorage.getItem("data");
+}
+showtask();
